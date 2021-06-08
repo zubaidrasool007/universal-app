2f4754c53a348c0bb8ca4134de7b3000819b4987
Zubaid.Dev <web.expert1221@gmail.com>
2021-06-08 18:07:35 +0500

    Meta title and meta description from browser platform

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { Title, Meta } from "@angular/platform-browser";
 
 @Component({
     selector: 'app-user',
-    templateUrl: './user.component.html'
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
-
+    constructor(
+        private titleService: Title,
+        private metaTagService: Meta
+    ) {
+        this.metaTagService.updateTag({'description':'Universal User page description' });
+        this.metaTagService.updateTag({'og:description':'Universal User page description' });
+        this.metaTagService.updateTag({'twitter:description':'Universal User page description' });
+        this.titleService.setTitle('Universal User Page')
+    }
 }
