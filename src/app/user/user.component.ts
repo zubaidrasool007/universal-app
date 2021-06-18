29a323a5412e7bf9edeaa3b3b2fd9681914caa0c
Zubaid.Dev <web.expert1221@gmail.com>
2021-06-18 17:19:31 +0500

    API Url and user meta tags API call

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { Title, Meta } from "@angular/platform-browser";
+import { take } from 'rxjs/operators';
+import { UserService } from "./services/user.service";
 
 @Component({
     selector: 'app-user',
@@ -7,13 +9,24 @@ import { Title, Meta } from "@angular/platform-browser";
     styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
+    userMetaData: any;
+
     constructor(
+        private userService: UserService,
         private titleService: Title,
         private metaTagService: Meta
     ) {
-        this.metaTagService.updateTag({'description':'Universal User page description' });
-        this.metaTagService.updateTag({'og:description':'Universal User page description' });
-        this.metaTagService.updateTag({'twitter:description':'Universal User page description' });
-        this.titleService.setTitle('Universal User Page')
+        this.userService.getUserMetaData().pipe(take(1)).subscribe(res => {
+            this.userMetaData = res;
+            this.metaTagService.updateTag({ 'description': this.userMetaData.description });
+            this.metaTagService.updateTag({ 'og:description': this.userMetaData.description });
+            this.metaTagService.updateTag({ 'twitter:description': this.userMetaData.description });
+            this.metaTagService.updateTag({ 'og:image': this.userMetaData.image });
+            this.titleService.setTitle(this.userMetaData.title);
+        });
+    }
+
+    decodeUri(url: string): string {
+        return decodeURIComponent(url);
     }
 }
